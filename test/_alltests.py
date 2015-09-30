ce0666b2ee985f0ca02db2f0a6b4e3d812d09830
Johan Jonkers <johan@seecr.nl>
2015-09-30 16:37:30 +0200

    JJ: license

--- test/_alltests.py
+++ test/_alltests.py
@@ -1,5 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+## begin license ##
+#
+# "Meresco Fetch" is a small frame work to build simple, custom harvesters.
+#
+# Copyright (C) 2015 Koninklijke Bibliotheek (KB) http://www.kb.nl
+# Copyright (C) 2015 Seecr (Seek You Too B.V.) http://seecr.nl
+#
+# This file is part of "Meresco Fetch"
+#
+# "Meresco Fetch" is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# "Meresco Fetch" is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with "Meresco Fetch"; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#
+## end license ##
 
 from seecrdeps import includeParentAndDeps                 #DO_NOT_DISTRIBUTE
 includeParentAndDeps(__file__, scanForDeps=True)           #DO_NOT_DISTRIBUTE
--- testsetup.sh
+++ testsetup.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 ## begin license ##
 #
-# "Meresco Html" is a template engine based on generators, and a sequel to Slowfoot.
-# It is also known as "DynamicHtml" or "Seecr Html".
+# "Meresco Fetch" is a small frame work to build simple, custom harvesters.
 #
 # Copyright (C) 2008-2009 Seek You Too (CQ2) http://www.cq2.nl
 # Copyright (C) 2012-2013, 2015 Seecr (Seek You Too B.V.) http://seecr.nl
+# Copyright (C) 2015 Koninklijke Bibliotheek (KB) http://www.kb.nl
 #
-# This file is part of "Meresco Html"
+# This file is part of "Meresco Fetch"
 #
-# "Meresco Html" is free software; you can redistribute it and/or modify
+# "Meresco Fetch" is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
 #
-# "Meresco Html" is distributed in the hope that it will be useful,
+# "Meresco Fetch" is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with "Meresco Html"; if not, write to the Free Software
+# along with "Meresco Fetch"; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #
 ## end license ##
